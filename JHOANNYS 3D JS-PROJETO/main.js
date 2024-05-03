3a4287f5813f0e4634fa4b6aecf13a27b8edd34c
jhoannysl <145028910+jhoannysl@users.noreply.github.com>
2024-05-02 22:30:07 -0300

    projeto js

--- JHOANNYS 3D JS-PROJETO/main.js
+++ JHOANNYS 3D JS-PROJETO/main.js
@@ -15,21 +15,13 @@ for(let i = 0; i <botoes.length;i++){
       }
       
       const contadores = document.querySelectorAll(".contador");
-      const tempoObjetivo1 = new Date("2024-10-05T00:00:00");
-      const tempoObjetivo2 = new Date("2024-12-05T00:00:00");
-      const tempoObjetivo3 = new Date("2024-12-30T00:00:00");
-      const tempoObjetivo4 = new Date("2025-10-01T00:00:00");
+      const tempoObjetivo1 = new Date("2020-10-05T00:00:00");
+      const tempoObjetivo2 = new Date("2023-12-05T00:00:00");
+      const tempoObjetivo3 = new Date("2023-12-30T00:00:00");
+      const tempoObjetivo4 = new Date("2024-02-01T00:00:00");
 
       const tempos = [tempoObjetivo1,tempoObjetivo2,tempoObjetivo3,tempoObjetivo4];
 
-      function atualizaCronometro(){
-         for (let i=0; i<contadores.length;i++){
-            contadores[i].textContent = calculatempo(tempos[0]);         
-      }  
-          }
-
-         atualizaCronometro();
-         setInterval(atualizaCronometro,1000);
 
       function calculatempo(tempoObjetivo){
          let tempoAtual = new Date();
@@ -42,6 +34,21 @@ for(let i = 0; i <botoes.length;i++){
          segundos %= 60;
          minutos %= 60;
          horas %= 24;
-
-         return dias + "dias" + horas + "horas" + minutos + "minutos" + segundos + "segundos";
+        if (tempoFinal>0){
+           return dias + "dias" + horas + "horas" + minutos + "minutos" + segundos + "segundos";
+      } else {
+         return "prazo finalizado";
+      }
 }
+
+      function Atualizacronometro(){
+         for (let i=0; i<contadores.length;i++){
+         contadores[0],textContent=calculaTempo(tempos[0]);
+        }
+      }
+
+      function comencaCronometro(){
+         AtualizaCronometro();
+         setInterval(AtualizaCronometro,1000);
+
+      }
